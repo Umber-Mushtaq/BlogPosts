e30fd539cd53f90667fb8fd8cdf62be30ca1a08c
Umber-Mushtaq <umber22j@gmail.com>
2025-10-15 15:01:28 +0500

    Front End Changings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Blogs from "./components/Blogs";
-import TrendingBlogs from "./components/TrendingBlogs";
-import RecentBlogs from "./components/RecentBlogs";
 import { PopUpProvider } from "./Context/PopUpContext";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [
-      { path: "blogs", element: <Blogs /> },
-      { path: "trending", element: <TrendingBlogs /> },
-      { path: "recent", element: <RecentBlogs /> },
-    ],
   },
 ]);
 
--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,17 +1,7 @@
-import { Outlet } from "react-router-dom";
-import SideNav from "../components/SideNav";
-import NavBar from "../components/NavBar";
-import Catagories from "../components/Catagories";
-
 const MainLayout = () => {
   return (
-    <div className='flex'>
-      <SideNav />
-      <div>
-        <NavBar />
-        <Catagories />
-        <Outlet />
-      </div>
+    <div>
+      <h1>Outlet</h1>
     </div>
   );
 };
